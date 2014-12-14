bb500d22b1463cff6999c3e98f3b63842b400de8
RogerChern <chenyuntao08@gmail.com>
2014-12-14 21:36:36 +0800

    fix some nomenclature issues

--- RC_STL/vector.h
+++ RC_STL/vector.h
@@ -8,50 +8,63 @@
 
 #ifndef RC_STL_vector_h
 #define RC_STL_vector_h
+#include <type_traits>
 
 namespace RC {
-    template <class T, class Allocator = allocator<T> >
+    template <typename T, typename Allocator = allocator<T> >
     class vector
     {
     public:
-        
         using value_type             = T;
         using allocator_type         = Allocator;
-        using reference              = allocator_type::reference;
-        using const_reference        = allocator_type::const_reference;
-        using iterator               = allocator_type::pointer;
-        using const_iterator         = allocator_type::pointer;
-        using size_type              = allocator_type::size_type;
-        using difference_type        = allocator_type::difference_type;
-        using pointer                = allocator_type::pointer;
-        using const_pointer          = allocator_type::const_pointer;
+        using reference              = typename allocator_type::reference;
+        using const_reference        = typename allocator_type::const_reference;
+        using iterator               = typename allocator_type::pointer;
+        using const_iterator         = typename allocator_type::pointer;
+        using size_type              = typename allocator_type::size_type;
+        using difference_type        = typename allocator_type::difference_type;
+        using pointer                = typename allocator_type::pointer;
+        using const_pointer          = typename allocator_type::const_pointer;
         using reverse_iterator       = std::reverse_iterator<iterator>;
         using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    private:
+        pointer _start   = nullptr;
+        pointer _end     = nullptr;
+        std::pair<allocator_type, pointer> _end_cap;
+    private:
+        allocator_type & alloc() noexcept {
+            return _end_cap.first();
+        }
+    public:
+        vector() noexcept(std::is_nothrow_default_constructible<allocator_type>::value);
+        ~vector() {
+            alloc().deallocate(_start, _end - _start);
+        }
         
-        vector()
-        noexcept(is_nothrow_default_constructible<allocator_type>::value);
         explicit vector(const allocator_type&);
-        explicit vector(size_type n);
+        explicit vector(size_type n) {
+            _start = allocator_type(n);
+            
+        }
         explicit vector(size_type n, const allocator_type&); // C++14
         vector(size_type n, const value_type& value, const allocator_type& = allocator_type());
         template <class InputIterator>
             vector(InputIterator first, InputIterator last, const allocator_type& = allocator_type());
         vector(const vector& x);
         vector(vector&& x)
-        noexcept(is_nothrow_move_constructible<allocator_type>::value);
-        vector(initializer_list<value_type> il);
-        vector(initializer_list<value_type> il, const allocator_type& a);
-        ~vector();
+        noexcept(std::is_nothrow_move_constructible<allocator_type>::value);
+        vector(std::initializer_list<value_type> il);
+        vector(std::initializer_list<value_type> il, const allocator_type& a);
         vector& operator=(const vector& x);
         vector& operator=(vector&& x)
         noexcept(
                  allocator_type::propagate_on_container_move_assignment::value &&
-                 is_nothrow_move_assignable<allocator_type>::value);
-        vector& operator=(initializer_list<value_type> il);
+                 std::is_nothrow_move_assignable<allocator_type>::value);
+        vector& operator=(std::initializer_list<value_type> il);
         template <class InputIterator>
             void assign(InputIterator first, InputIterator last);
         void assign(size_type n, const value_type& u);
-        void assign(initializer_list<value_type> il);
+        void assign(std::initializer_list<value_type> il);
         
         allocator_type get_allocator()   const noexcept;
         
@@ -103,7 +116,7 @@ namespace RC {
         iterator insert(const_iterator position, size_type n, const value_type& x);
         template <class InputIterator>
             iterator insert(const_iterator position, InputIterator first, InputIterator last);
-        iterator insert(const_iterator position, initializer_list<value_type> il);
+        iterator insert(const_iterator position, std::initializer_list<value_type> il);
         
         iterator erase(const_iterator position);
         iterator erase(const_iterator first, const_iterator last);
@@ -115,7 +128,7 @@ namespace RC {
         
         void swap(vector&)
         noexcept(!allocator_type::propagate_on_container_swap::value ||
-                 __is_nothrow_swappable<allocator_type>::value);
+                 std::__is_nothrow_swappable<allocator_type>::value);
         
         bool __invariants() const;
     };
